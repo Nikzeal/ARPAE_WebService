5dd25a6397e9b909c6d437e9696336c6e32ed13b
danie <danie@192.168.1.70>
2023-04-18 17:50:17 +0200

    Setuppato la servlet AccessoUtenti.java e completato i controlli sulla validità dell'username, la lunghezza della password e la presenza di campi vuoti. Ho aggiunto i commenti dove manca il codice da implementare.

---
+++ ARPAE_webservices/src/main/java/container/AccessoUtenti.java
@@ -0,0 +1,83 @@
+package container;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Servlet implementation class AccessoUtenti
+ */
+@WebServlet("/AccessoUtenti")
+public class AccessoUtenti extends HttpServlet {
+	
+	private static final long serialVersionUID = 1L;
+    String username = "";
+    String password = "";
+    String risposta;
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public AccessoUtenti() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+    //METODI CONTROLLO CAMPI
+    //Username check
+    public boolean isValidUsername(String username) {
+    	
+    	if((username == null) || username.contains(" ")) {
+    		return false;
+    	}else
+    		return true;
+    }
+    //Password check
+    public boolean isValidPassword(String password) {
+    	if(password.length() <= 8)
+    		return false;
+    	else
+    		return true;
+    }
+    //Empty input check
+    public boolean isNotEmpty() {
+ 	   if((username == "") || (password == ""))
+ 		   return false;
+ 	   else
+ 		   return true;	   
+    }
+    
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		/*IMPLEMENTAZIONE METODO DO POST
+		 * Output writer - input reader
+		 * Acquisizione valore campi del form
+		 * manipolazione stringa del messaggio
+		 */
+		
+		/* i controlli andranno organizzati cosi:
+			 * se i seguenti controlli passano
+			 * metodo controlloValiditaPassword()
+			 * metodo controlloValiditaUsername()
+			 * metodo ControlloVerificaMail()
+			 * metodo controlloCampiVuoti()
+			 * allora si potra passare al codice successivo
+			 */
+		
+		//Controlli con database
+	}
+
+}
